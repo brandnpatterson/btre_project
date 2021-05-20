e5caf4e5b54ad779d6806b496615e11f67cdcdb6
Brandon Patterson <brandnpatterson@gmail.com>
2021-05-19 21:43:54 -0500

    make home, about, and listing pages dynamic

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
-    listings = Listing.objects.all()
-    paginator = Paginator(listings, 3)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     page_listings = paginator.get_page(page)
 
@@ -15,7 +15,12 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
