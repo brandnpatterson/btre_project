721cc3271d919a2aa9e3f4996818631923685eff
Brandon Patterson <brandnpatterson@gmail.com>
2021-06-02 14:02:01 -0500

    chapter 8: accounts & authentication

--- btre/settings.py
+++ btre/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import environ
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 env = environ.Env()
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -145,3 +147,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,2 +1,12 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+setTimeout(function () {
+  $('#message').fadeOut('slow');
+}, 3000);
+
+var logout = document.querySelector('.js-logout');
+
+logout.addEventListener('click', function () {
+  document.querySelector('.js-logout-form').submit();
+});
